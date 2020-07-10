5e75b7c45168b70cdc858a046990a7be657fe462
Saif Shakil Shaikh <me.shaikhsaif@outlook.com>
2020-07-10 08:32:40 +0530

    Simple Calculator Completed.

---
+++ SimpleCalculator/InputConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    public class InputConverter
+    {
+        public double ConvertInputToNumeric(string argTextinput) {
+            double convertedNumber;
+            if (!double.TryParse(argTextinput, out convertedNumber)) throw new ArgumentException("Expected a numeric value");
+                return convertedNumber;
+        }
+    }
+}
--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -4,9 +4,30 @@ namespace SimpleCalculator
 {
     class Program
     {
-        static void Main(string[] args)
+         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                InputConverter inputConverter = new InputConverter();
+                CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+                Console.Write("Please Enter First Number: ");
+                double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                Console.Write("Please Enter First Number: ");
+                double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                Console.WriteLine("Please Enter Operation: ");
+                Console.WriteLine("Follow the prefix for operation: (+) or (add), (-) or (subtract), (*) or (multiply) and (/) or (divide) ");
+                string operation = Console.ReadLine();
+
+                double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                //In real world we would want to log this message.
+                Console.WriteLine(ex.Message);
+            }
+
             Console.ReadLine();
         }
     }
